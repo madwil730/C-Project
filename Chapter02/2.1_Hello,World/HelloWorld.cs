474ee81b7884eb9d5f13b8099057ab8f900fd31c
SangUK <madwil@naver.com>
2023-07-01 15:18:36 +0900

    Chapter02 공부 내용 커밋

---
+++ Chapter02/2.1_Hello,World/HelloWorld.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.LogFormat("Hello {0}!","World");
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
