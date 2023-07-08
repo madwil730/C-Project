a7be1a31273293143115155dc00453a5d64f94d4
SangUK <madwil@naver.com>
2023-07-09 00:51:30 +0900

    3.9 공부 내용 커밋

--- Chapter03/3.9 문자열 다루기/Chapter_3.9.md
+++
@@ -1,7 +0,0 @@
-# 공유 형식 시스템 
-.Net을 지원하는 모든 언어에서 사용하는 데이터 형식 체계  
-예)  
-System.Byte => C#: byte , c++: unsigned char   
-System.Int32 => C#: int , c++: int 또는 long   
-
-
---
+++ Chapter03/3.9 문자열 다루기/StringSearch.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StringSearch : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string str = "Good Morning";
+
+        Debug.Log(str.IndexOf("o"));
+        Debug.Log(str.LastIndexOf("o"));
+        Debug.Log(str.StartsWith("Good"));
+        Debug.Log(str.EndsWith("Good"));
+        Debug.Log(str.Contains("Morning"));
+        Debug.Log(str.Replace("Good", "Bye"));
+
+        Debug.Log("ABC".ToLower());
+        Debug.Log("abc".ToUpper());
+        Debug.Log("Happt Friday".Insert(5,"Sunny"));
+        Debug.Log("I Don't Love You".Remove(2,6));
+        Debug.Log(" No Spaces ".Trim());
+        Debug.Log(" No Spaces ".TrimStart());
+        Debug.Log(" No Spaces ".TrimEnd());
+
+        Debug.Log(str.Substring(0,5));
+        Debug.Log(str.Substring(5));
+
+        string[] arr = str.Split(new string[] { " " }, System.StringSplitOptions.None);
+
+        foreach(string element in arr)
+            Debug.Log(element);
+
+        string fmt = "{0,-20}{1,-15}{2,30}";
+
+        Debug.LogFormat(fmt, "aaa", "bbb", "ccc");
+		Debug.LogFormat("item_count : {0}, hp : {1}, name : {2}", 10, 2, "tex");
+		Debug.LogFormat("{0:D}",123);
+        Debug.LogFormat("{0:X}",123);
+
+        int num = 3;
+
+        Debug.Log($"{num} is number");
+
+
+
+
+
+    }
+
+
+}
