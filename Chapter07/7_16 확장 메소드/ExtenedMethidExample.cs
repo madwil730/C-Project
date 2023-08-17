f99e4d7176646618f32609ec40585a51aaff32b2
SangUK <madwil@naver.com>
2023-08-17 21:57:59 +0900

    ExtenedMethidExample.cs  커밋

---
+++ Chapter07/7_16 확장 메소드/ExtenedMethidExample.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+
+public static class IntergerExtension
+{
+    public static int Square(this int myInt)
+    {
+        return myInt * myInt;
+    }
+
+	public static int Power(this int myInt, int exponent)
+	{
+        int result = myInt;
+        for(int i =1; i <exponent; i++)
+            result *= myInt; 
+		return result;
+	}
+
+}
+
+public class ExtenedMethidExample : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(3.Square());
+        Debug.Log(3.Power(4));
+    }
+
+   
+}
